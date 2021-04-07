a89198dd5f5580ff5b7e1d99f0fdbf40303ebf07
Martin <=>
2021-04-07 12:59:11 +0200

    Added prize values to the prizelist

--- QuizGame/MainWindow.xaml.cs
+++ QuizGame/MainWindow.xaml.cs
@@ -20,9 +20,39 @@ namespace QuizGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<int> prizeList;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitData();
+            InitPrizeList();
+        }
+
+        // Initialize global variables at the start
+        private void InitData()
+        {
+            prizeList = new List<int>();
+        }
+
+        // Initialize the list of prizes with values
+        // sets the itemsource for the prize list
+        private void InitPrizeList()
+        {
+            prizeList.Add(500);
+            prizeList.Add(1000);
+            prizeList.Add(2000); // fixed prize
+            prizeList.Add(5000);
+            prizeList.Add(10000);
+            prizeList.Add(20000);
+            prizeList.Add(50000); // fixed prize
+            prizeList.Add(75000);
+            prizeList.Add(150000);
+            prizeList.Add(250000);
+            prizeList.Add(500000);
+            prizeList.Add(1000000); // fixed prize
+
+            lbPrizes.ItemsSource = prizeList;
         }
     }
 }
