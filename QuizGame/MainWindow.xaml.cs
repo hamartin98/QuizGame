a4fe925f4b74675c414e0c7449f95c31f7ee734c
hamartin98 <hazi.martin@gmail.com>
2021-04-08 21:46:42 +0200

    Moved prize list and all related functionaility to the PrizeListControl class Implemented answer checking and progressing

--- QuizGame/MainWindow.xaml.cs
+++ QuizGame/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Windows.Controls;
 using System.Net;
 using System.Web;
 using System.Net.Http;
+using System;
 
 namespace QuizGame
 {
@@ -13,16 +14,14 @@ namespace QuizGame
     /// </summary>
     public partial class MainWindow : Window
     {
-        private List<int> prizeList; // Store the list of prizes
         private List<AnswerButton> answerButtons; // Store the answer buttons
         private List<QuestionData> questionList; // Store the questions
         private int currentQuestionIdx; // The index of the current question
-
+        
         public MainWindow()
         {
             InitializeComponent();
             InitData();
-            InitPrizeList();
             InitAnswerButtons();
         }
 
@@ -36,30 +35,9 @@ namespace QuizGame
         // Initialize member variables at the start
         private void InitData()
         {
-            prizeList = new List<int>();
             answerButtons = new List<AnswerButton>();
             questionList = new List<QuestionData>();
             currentQuestionIdx = 0;
-        }
-
-        // Initialize the list of prizes with values
-        // sets the itemsource for the prize list
-        private void InitPrizeList()
-        {
-            prizeList.Add(500);
-            prizeList.Add(1000);
-            prizeList.Add(2000); // fixed prize
-            prizeList.Add(5000);
-            prizeList.Add(10000);
-            prizeList.Add(20000);
-            prizeList.Add(50000); // fixed prize
-            prizeList.Add(75000);
-            prizeList.Add(150000);
-            prizeList.Add(250000);
-            prizeList.Add(500000);
-            prizeList.Add(1000000); // fixed prize
-
-            lbPrizes.ItemsSource = prizeList;
         }
 
         // Initialize and add the answer buttons to the ui and store them in the answerButtons list
@@ -72,7 +50,7 @@ namespace QuizGame
             {
                 labelText = ((char)(65 + idx)).ToString(); // Ascii A is 65
                 ansBtn = new AnswerButton(labelText);
-                ansBtn.Margin = new Thickness(10);
+                ansBtn.Margin = new Thickness(5);
                 ansBtn.Click += AnswerButton_Click;
 
                 answerStack.Children.Add(ansBtn);
@@ -84,7 +62,6 @@ namespace QuizGame
         private void AnswerButton_Click(object sender, RoutedEventArgs e)
         {
             string answer = (sender as AnswerButton).GetText();
-            MessageBox.Show(answer);
             CheckAnswer(answer);
         }
 
@@ -102,6 +79,9 @@ namespace QuizGame
             currentQuestion.DecodeData(); // need to decode data before usage
             lblQuestion.Text = currentQuestion.Question;
 
+            // For testing only
+            Console.WriteLine(currentQuestion.Correct_answer);
+
             for(int idx = 0; idx < 4; idx++)
             {
                 answerButtons[idx].SetData(currentQuestion.GetAnswers()[idx]);
@@ -111,20 +91,16 @@ namespace QuizGame
         // Checks the selected answer
         private void CheckAnswer(string answer)
         {
-            MessageBox.Show(questionList[currentQuestionIdx].ToString());
-
             if(questionList[currentQuestionIdx].IsCorrect(answer))
             {
-                MessageBox.Show("Correct answer!");
                 currentQuestionIdx++;
                 NextQuestion();
-                // Step on the prize list
+                prizeList.StepUp();
             }
             else
             {
-                MessageBox.Show("Wrong answer, end of the game!");
-                // End of the game
-                // If there is a fix prize, the player gets it
+                MessageBox.Show($"Wrong answer, end of the game!\nYour prize: {prizeList.GetPrize(true)}");
+                // Show Endgame screen
                 // Save result
             }
         }
