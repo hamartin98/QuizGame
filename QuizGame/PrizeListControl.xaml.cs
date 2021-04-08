4345058436788772b859eecb72911ada5b94e7d2
hamartin98 <hazi.martin@gmail.com>
2021-04-08 21:56:51 +0200

    Moved prize list and all related functionaility to the PrizeListControl class Implemented answer checking and progressing

---
+++ QuizGame/PrizeListControl.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizGame
+{
+    /// <summary>
+    /// Interaction logic for PrizeListControl.xaml
+    /// </summary>
+    public partial class PrizeListControl : UserControl
+    {
+        private List<int> prizeList;
+        private int currentStep = -1;
+        private int fixedPrize = 0;
+        private List<PrizeListItem> prizeListItems;
+
+        public PrizeListControl()
+        {
+            InitializeComponent();
+            AddListItems();
+        }
+
+        private void InitPrizeList()
+        {
+            prizeList = new List<int>();
+
+            prizeList.Add(500);
+            prizeList.Add(1000);
+            prizeList.Add(2000);
+            prizeList.Add(5000); // new fixed prize
+            prizeList.Add(10000);
+            prizeList.Add(20000);
+            prizeList.Add(50000);
+            prizeList.Add(75000); // new fixed prize
+            prizeList.Add(150000);
+            prizeList.Add(250000);
+            prizeList.Add(500000);
+            prizeList.Add(1000000); // fixed prize
+        }
+
+        private void AddListItems()
+        {
+            InitPrizeList();
+
+            prizeListItems = new List<PrizeListItem>();
+            PrizeListItem prizeListItem;
+            bool isFixed = false;
+
+            for (int idx = 0; idx < prizeList.Count; idx++)
+            {
+                isFixed = (idx == 3 || idx == 7 || idx == 11) ? true : false;
+                prizeListItem = new PrizeListItem(idx + 1, prizeList[idx], isFixed);
+                prizePanel.Children.Add(prizeListItem);
+                prizeListItems.Add(prizeListItem);
+            }
+        }
+        
+        // Step to the next prize
+        public void StepUp()
+        {
+            currentStep++;
+
+            if(currentStep == 3 || currentStep == 7 || currentStep == 11)
+            {
+                fixedPrize = prizeList[currentStep];
+            }
+
+            prizeListItems[currentStep].ChangeStateToReached();
+        }
+
+        // Returns the prize of the player based on the criteria
+        // If the player lost, returns the last fixed value, otherwise the player stopped and gets the prize
+        public int GetPrize(bool lost)
+        {
+            if(!lost)
+            {
+                return prizeList[currentStep];
+            }
+
+            return fixedPrize;
+        }
+    }
+}
---
+++ QuizGame/PrizeListItem.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuizGame
+{
+    /// <summary>
+    /// Interaction logic for PrizeListItem.xaml
+    /// </summary>
+    public partial class PrizeListItem : UserControl
+    {
+        public int Prize { get; } // Amount of the prize
+        public bool IsFixed { get; } = false; // If it's true, the player wins this money if once it's reached
+
+        public enum State { DEFAULT, REACHED }
+        private State state;
+
+        public PrizeListItem(int index, int prize, bool isFixed)
+        {
+            InitializeComponent();
+
+            Prize = prize;
+            this.IsFixed = isFixed;
+            state = State.DEFAULT;
+            SetUiData(index);
+        }
+
+        // Sets the values on the UI
+        private void SetUiData(int index)
+        {
+            if(IsFixed)
+            {
+                Foreground = Brushes.White;
+            }
+            tbIndex.Text = $"{index}.";
+            decimal prizeAsDecimal = Convert.ToDecimal(Prize);
+            tbPrize.Text = prizeAsDecimal.ToString("C1", CultureInfo.CreateSpecificCulture("en-Us"));
+        }
+
+        // Change the state of the current prizeList item
+        public void ChangeStateToReached()
+        {
+            this.state = State.REACHED;
+            UpdateUI();
+        }
+
+        // Update the UI based on the stated
+        private void UpdateUI()
+        {
+            if(state == State.REACHED)
+            {
+                rectReached.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
